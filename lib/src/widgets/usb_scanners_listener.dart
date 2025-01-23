912f34e410852d07245139294e58fad0b11c2769
KickNext <nikitiser@gmail.com>
2025-01-23 19:52:46 +0500

    Revert "Обновление контроллера потоков в UsbScannersListener и изменение версии пакета на 0.1.3"
    This reverts commit 694430b228d7b99986d9aad7d1e073ffb9e213bd.

--- lib/src/widgets/usb_scanners_listener.dart
+++ lib/src/widgets/usb_scanners_listener.dart
@@ -19,9 +19,7 @@ class UsbScannersListener extends StatefulWidget {
 }
 
 class _UsbScannersListenerState extends State<UsbScannersListener> {
-  StreamSubscription<String>? _subscription;
-  static StreamController<String>? _controller;
-  static int _listenerCount = 0;
+  late StreamSubscription<String> _subscription;
 
   @override
   void initState() {
@@ -30,38 +28,14 @@ class _UsbScannersListenerState extends State<UsbScannersListener> {
   }
 
   void _startListening() {
-    if (_controller == null || _controller!.isClosed) {
-      _controller = StreamController<String>.broadcast(
-        onListen: () {
-          _listenerCount++;
-          debugPrint("UsbScannersListener count: $_listenerCount");
-        },
-        onCancel: () {
-          _listenerCount--;
-          debugPrint("UsbScannersListener count: $_listenerCount");
-          if (_listenerCount == 0) {
-            _controller!.close();
-            _controller = null;
-          }
-        },
-      );
-
-      // Перенаправляем данные из оригинального потока в наш контроллер
-      UsbSerialScannersManager.scanDataStream.listen((data) {
-        if (_controller != null && !_controller!.isClosed) {
-          _controller!.add(data);
-        }
-      });
-    }
-
-    _subscription = _controller!.stream.listen((data) {
+    _subscription = UsbSerialScannersManager.scanDataStream.listen((data) {
       widget.onScan(data);
     });
   }
 
   @override
   void dispose() {
-    _subscription?.cancel();
+    _subscription.cancel();
     super.dispose();
   }
 
